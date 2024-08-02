4817319a9b6d1a2fcab8bb411da8e724fbcfbffa
BasmaElshihaby <basmaelshihaby@gmail.com>
2024-08-02 19:10:19 +0300

    333333333

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,15 @@
-SELECT *
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE table_name = 'books'
+-- task_4.sql
+USE alx_book_store;
+
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'books';
